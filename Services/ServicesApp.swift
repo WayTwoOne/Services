7fbb87e1e712aa00c050aa94f6e09fbe199b8a8c
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-03-29 03:27:59 -0700

    Initial Commit

---
+++ Services/ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesApp.swift
+//  Services
+//
+//  Created by Vladimir on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
